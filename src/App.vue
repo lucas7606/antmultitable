63b84c12c7b0e421e1d8690d7ac781d960a97e0d
lucas7606 <13811537606@163.com>
2023-12-08 14:50:41 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<!--
+ * @Author: lucas 13811537606@163.com
+ * @Date: 2023-12-08 12:07:22
+ * @LastEditors: lucas 13811537606@163.com
+ * @LastEditTime: 2023-12-08 13:41:27
+ * @FilePath: /antMultiTable/src/App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App345345345"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
